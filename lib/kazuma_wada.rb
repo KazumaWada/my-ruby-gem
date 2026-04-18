02e46e174c38998e58c8ff5bbd90f61963276bdb
KazumaWada <kazumawadaa@gmail.com>
2026-04-18 23:11:44 +0900

    Initial commit

---
+++ lib/kazuma_wada.rb
@@ -0,0 +1,11 @@
+require "kazuma_wada/version"
+
+module KazumaWada
+  def self.profile
+    puts "===================="
+    puts " Name   : kazuma_wada"
+    puts " Age    : 28"
+    puts " Height : 182cm"
+    puts "===================="
+  end
+end
---
+++ lib/kazuma_wada/version.rb
@@ -0,0 +1,3 @@
+module KazumaWada
+  VERSION = "0.1.0"
+end
